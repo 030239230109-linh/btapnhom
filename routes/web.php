35982fa32bc64b0b9b07b3c38193382b19254ff4
LeHoangAn-MIS <030239230002@st.buh.edu.vn>
2026-04-21 14:38:40 +0700

    Câu 2

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController2;
 
 Route::get('/', [HomeController::class, 'index']);
 
@@ -12,3 +13,7 @@ Route::get('/dashboard', function () {
 
 
 require __DIR__.'/auth.php';
+
+Route::get('/laptop', [HomeController2::class, 'laptop']);
+Route::get('/laptop/theloai/{id}', [HomeController2::class, 'theloai']);
+Route::get('/laptop/loc', [HomeController2::class, 'loc']);
