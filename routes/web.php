278a735ce5af679c07733889613e595a62a5e37b
Thuylinh117 <030239230111@st.buh.edu.vn>
2026-04-21 14:41:17 +0700

    Câu 5 - Tìm kiếm

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController5;
+
 
 Route::get('/', [HomeController::class, 'index']);
 
@@ -9,6 +11,7 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-
+//Câu 5: Tìm kiếm laptop
+Route::get('timkiem', [HomeController5::class, 'search'])->name('laptop.search');
 
 require __DIR__.'/auth.php';
