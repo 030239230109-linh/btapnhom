288aececdb677d52ed01aa50e05aa5b6e1e5501d
meillinhst-cpu <meillinh.st@gmail.com>
2026-04-21 15:08:46 +0700

    cau 7

--- routes/web.php
+++ routes/web.php
@@ -10,5 +10,10 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 
+use App\Http\Controllers\LaptopController7;
 
+Route::get('/home', [LaptopController7::class, 'index'])->name('home');
+
+// lọc theo hãng
+Route::get('/brand/{id}', [LaptopController7::class, 'filterByBrand'])->name('brand');
 require __DIR__.'/auth.php';
